0a60bf9a5b658e5d7a28e4e61d7d9c714b544862
antarikshray <antarikshray@gmail.com>
2022-04-17 03:25:16 +0530

    Antariksh: Logic add for different order in object

--- assignment_3/index.js
+++ assignment_3/index.js
@@ -1,14 +1,28 @@
-const compare = (firstObject, secondObject) => {
-    const firstObjectAray = Object.values(firstObject);
-    const secondObjectAray = Object.values(secondObject);
+const compareWithOrder = (firstObject, secondObject) => {
+    const firstObjectArray = Object.values(firstObject);
+    const secondObjectArray = Object.values(secondObject);
 
-    if (firstObjectAray.length !=secondObjectAray.length)
+    if (firstObjectArray.length !=secondObjectArray.length)
         return false;
 
-    for(let i=0;i<firstObjectAray.length; i++){
-        if(typeof(firstObjectAray[i]) === 'object' && typeof(secondObjectAray[i]) === 'object')
-            return compare(firstObjectAray[i], secondObjectAray[i]);
-        if(firstObjectAray[i] != secondObjectAray[i])
+    for(let i=0;i<firstObjectArray.length; i++){
+        if(typeof(firstObjectArray[i]) === 'object' && typeof(secondObjectArray[i]) === 'object')
+            return compareWithOrder(firstObjectArray[i], secondObjectArray[i]);
+        if(firstObjectArray[i] != secondObjectArray[i])
+            return false;
+    }
+    return true;
+}
+
+const compareWithoutOrder = (firstObject, secondObject) => {
+
+    if (Object.keys(firstObject).length !=Object.keys(secondObject).length)
+        return false;
+
+    for(key in firstObject){
+        if(typeof(firstObject[key]) === 'object' && typeof(secondObject[key]) === 'object')
+            return compareWithoutOrder(firstObject[key], secondObject[key]);
+        if(firstObject[key] != secondObject[key])
             return false;
     }
     return true;
@@ -38,4 +52,18 @@ const b = {
     }
 }
 
-console.log(compare(a, b));
+console.log(compareWithOrder(a, b));
+
+const c = {
+    b: 1,
+    a: 2,
+    c:{
+        df: {
+            ra:1
+        },
+        ca: 1,
+        cd: 'Hello'
+    }
+}
+
+console.log(compareWithoutOrder(a, c));
