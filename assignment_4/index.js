4d6bc53a5da8e1cf28677f349dde013f8eeb171a
antarikshray <antarikshray@gmail.com>
2022-04-17 04:16:10 +0530

    Antariksh: Add logic for own stringify

--- assignment_4/index.js
+++ assignment_4/index.js
@@ -1,3 +1,25 @@
+const stringify = (object, iteration=0) => {
+    let res = "{\n";
+    iteration++;
+    let tabs="";
+    for(let i=0; i<iteration ;i++)
+        tabs+="    ";
+    for(key in object){
+        if(typeof(object[key])=='object')
+            res+=tabs+String(key)+":"+stringify(object[key], iteration)+tabs;
+        else{
+            if(key === Object.keys(object)[Object.keys(object).length-1])
+                res+=tabs+String(key)+":"+String(object[key]);
+            else
+                res+=tabs+String(key)+":"+String(object[key])+"\n";
+        }
+    }
+    tabs="";
+    for(let i=0; i<iteration-1 ;i++)
+        tabs+="    ";
+    return res+"\n"+tabs+"}";
+}
+
 const firstObject = {
     b:1,
     d: {
@@ -21,3 +43,5 @@ const secondObject = {
 }
 
 console.log(JSON.stringify(firstObject) === JSON.stringify(secondObject));
+console.log(stringify(firstObject));
+console.log(stringify(firstObject) === stringify(secondObject));
